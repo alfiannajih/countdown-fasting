081528af045ab0e367a530b6e18eaed250d08240
alfian <alfianhayato@yahoo.co.id>
2024-04-14 22:27:43 +0700

    grouping into one endpoint

--- backend/main.py
+++ backend/main.py
@@ -60,18 +60,25 @@ async def search_time_zone(timezone):
 
 @app.get("/api/timings")
 async def timings(query: str):
-    response = await search_timings(query)
-    
-    return response
+    response = {}
 
-@app.get("/api/reverse_location")
-async def reverse_location(lat, lon):
-    response = await reverse_geo_loc(lat, lon)
+    timings_response = await search_timings(query)
+    response["timings"] = {
+        "Maghrib": timings_response["timings"]["Maghrib"],
+        "Fajr": timings_response["timings"]["Fajr"]
+    }
     
-    return response
+    response["timezone"] = timings_response["meta"]["timezone"]
+    response["coordinates"] = {
+        "latitude": timings_response["meta"]["latitude"],
+        "longitude": timings_response["meta"]["longitude"]
+    }
 
-@app.get("/api/time_zone")
-async def time_zone(timezone):
-    response = await search_time_zone(timezone)
-    
+    timezone_response = await search_time_zone(response["timezone"])
+    response["timezone_offset"] = timezone_response["timezone_offset"]
+
+    reverse_geo_loc_response = await reverse_geo_loc(response["coordinates"]["latitude"], response["coordinates"]["longitude"])
+    display_location = reverse_geo_loc_response["display_name"].split(",")
+    response["display_location"] = f"{display_location[0]}, {display_location[1]} ({response['timezone']})"
+
     return response
